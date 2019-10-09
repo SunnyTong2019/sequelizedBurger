47f1da55085e5ee36024b6aa890bb845d933d076
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-10-08 22:17:40 -0700

    updated controller and app works as expected

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,30 +1,32 @@
-var express = require("express");
-var router = express.Router();
-var burger = require("../models/burger");
+var db = require("../models");
+
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        db.Burger.findAll({}).then(function (data) {
+            res.render("index", { burgers: data });
+        });
+    });
 
 
-router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
-        res.render("index", { burgers: data });
-    })
-});
+    app.post("/api/burgers", function (req, res) {
+        db.Burger.create({ burger_name: req.body.burger }).then(function (result) {
+            res.json({ id: result.insertId });
+        });
+    });
 
 
-router.post("/api/burgers", function (req, res) {
-    burger.insertOne(req.body.burger, function (result) {
-        res.json({ id: result.insertId });
-    })
-});
+    app.put("/api/burgers/:id", function (req, res) {
+        db.Burger.update(
+            { devoured: true },
+            { where: { id: req.params.id } }
+        ).then(function (result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+    });
 
-
-router.put("/api/burgers/:id", function (req, res) {
-    burger.updateOne(req.params.id, function (result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    })
-});
-
-module.exports = router;
+}
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(routes);
+routes(app);
 
 db.sequelize.sync().then(function () {
   app.listen(PORT, function() {
